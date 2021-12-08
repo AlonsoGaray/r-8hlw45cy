5c73e7feac5c217e50a0a71f40c9a94f4ea9ba6e
AlonsoGaray <alonsogarayp@gmail.com>
2021-12-08 13:29:33 -0500

    Checkbox show/hide

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    );
+const App = () => {
+  const [ checked, setChecked ] = useState(false)
+  const textToShow = () => {
+    setChecked(!checked)
   }
+
+  return (
+    <div className="wrapper">
+      <label><input onChange={textToShow} type="checkbox" checked={checked} /> Mostrar información importante</label>
+      { checked ? 
+      (<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>)
+      : (null)
+      }
+    </div>
+  );
 }
 
 export default App;
